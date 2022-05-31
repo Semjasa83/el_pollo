b6966d9231b3e431c0a81a0a2995e07839bd0d12
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-05-31 12:54:03 +0200

    favicon and first preps

--- js/game.js
+++ js/game.js
@@ -1,13 +1,15 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject(); //ist ein JSON das die ganzen Attribute schon besitzt durch die andere JS datei.
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d'); //damit context über CTX im schnellzugriff ist // ctx ist standard dafür
 
+    console.log('My Character is', character);
 
+    /*
     character.src = '../img/2.Secuencias_Personaje-Pepe-correcciขn/2.Secuencia_caminata/W-21.png'; //character wird ein bild zugeordnet
-
     ctx.drawImage(character, 20,20,50,150); //x,y koordinate, breite, höhe
+    */
 }
---
+++ models/movable-object.class.js
@@ -0,0 +1,10 @@
+class MovableObject { //an sich eine Schablone mit den Feldern definiert
+    x = 120; //definierter Startpunkt x und y
+    y = 400;
+    img;
+
+    moveRight() {   //function moveRight() -> moveRight(), kann so gekürzt werden.
+        console.log('Moving right');
+        
+    }
+}
