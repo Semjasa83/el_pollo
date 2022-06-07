803de8919617ffe9d68d4d05316f41e244ba5257
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-06-08 01:43:16 +0200

    wide Background

--- models/world.class.js
+++ models/world.class.js
@@ -14,12 +14,17 @@ class World {
         new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719),
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719),
     ];
 
     //.... -> =0
     canvas;
     ctx;
     keyboard;
+    camera_x = 0; //verschieben der Spielwelt
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -40,10 +45,15 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.translate(this.camera_x, 0); //Ausschnitt verschieben
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
         this.addToMap(this.character);
+
+        this.ctx.translate(-this.camera_x, 0); //am ende Auschnitt gegen korrigieren, sonst wäre Bild schwarz
 
         let self = this;
         requestAnimationFrame(function () {
