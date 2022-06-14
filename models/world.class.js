64792cf886e7385b08106f3ff83b7e0fffd487fd
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-06-14 23:57:31 +0200

    reactivate Background

--- models/world.class.js
+++ models/world.class.js
@@ -48,7 +48,7 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0); //Ausschnitt verschieben
-        //this.loopBackgroundsToMap();
+        this.loopBackgroundsToMap();
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
         this.addToMap(this.character);
