e648a1abf060ed9c8ac449bdf424fc40e15933a5
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-07-21 20:18:55 +0200

    buggy pics

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -13,10 +13,16 @@ class Chicken extends MovableObject {
     };
 
     IMAGES_WALKING = [
-        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png',
-        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita (estas salen por orden de la gallina gigantona)/2-Ga_centro.png',
-        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita (estas salen por orden de la gallina gigantona)/3.Ga_paso izquierdo.png'
+        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita_(estas_salen_por_orden_de_la_gallina_gigantona)/1.Ga_paso_derecho.png',
+        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita_(estas_salen_por_orden_de_la_gallina_gigantona)/2-Ga_centro.png',
+        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita_(estas_salen_por_orden_de_la_gallina_gigantona)/3.Ga_paso_izquierdo.png'
+        /*'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita_(estas_salen_por_orden_de_la_gallina_gigantona)/4.G_muerte.png'*/
     ];
+
+    IMAGES_DEAD = [
+        'img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita_(estas_salen_por_orden_de_la_gallina_gigantona)/4.G_muerte.png'
+    ];
+
     chicken_kill_sound = new Audio('audio/chicken.mp3');
 
     /**
@@ -25,23 +31,40 @@ class Chicken extends MovableObject {
     * @param {speed} - sets speed for Chicken, with random Speed
     */
     constructor() {
-        super().loadImage('img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
+        super().loadImage('img/3.Secuencias_Enemy_b_sico/Versi¢n_Gallinita_(estas_salen_por_orden_de_la_gallina_gigantona)/1.Ga_paso_derecho.png');
         this.loadImages(this.IMAGES_WALKING);
         this.x = 450 + Math.random() * 3800; //sets Spawn for Chickens
         this.speed = 0.3 + Math.random() * 0.35; //sets Speed for Chickens
-
         this.animate();
     }
 
     animate() {
+        this.chickenMove();
+        this.chickenAnimation();
+        this.chickenSpawn();
+    }
+
+    chickenMove() {
         setInterval( () => { // for moving left the Chickens
+            if (this.energy <= 0) {
+                this.speed = 0;
+            } else {
             this.x -= this.speed;
+            }
         }, 1000 / 60);
+    }
 
+    chickenAnimation() {
         setInterval(() => {
+            if(this.energy <= 0) {
+            this.playAnimation(this.IMAGES_DEAD);
+        } else {
             this.playAnimation(this.IMAGES_WALKING);
+        }
         }, 250);
+    }
 
+    chickenSpawn() {
         setInterval(() => {
             if (this.x > -100) {
                 this.x -= this.speed;
@@ -51,4 +74,6 @@ class Chicken extends MovableObject {
             }
         }, 1000 / 60);
     }
+
+
 }
--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,15 +1,15 @@
-class ThrowableObject extends MovableObject{
+class ThrowableObject extends MovableObject {
     throw_sound = new Audio('audio/throw.mp3');
     bottle_sound = new Audio('audio/bottle.mp3');
-    
+
     IMAGES_ROTATEBOTTLE = [
-     'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_3.png',
-     'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_4.png',
-     'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_5.png',
-     'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_6.png'
+        'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_3.png',
+        'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_4.png',
+        'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_5.png',
+        'img/6.botella/Rotaciขn/Mesa_de_trabajo_1_copia_6.png'
     ];
-    
-    
+
+
     constructor(x, y) {
         super().loadImage('img/7.Marcadores/Icono/Botella.png');
         this.loadImages(this.IMAGES_ROTATEBOTTLE);
@@ -22,18 +22,18 @@ class ThrowableObject extends MovableObject{
         this.throw_sound.play();
         this.bottle_sound.play();
     }
-    
+
     /**
      * Range and Speed from Bottles
      */
-    throw(){
+    throw() {
         this.speedY = 10;
         this.applyGravity();
         setInterval(() => {
             this.x += 45;
         }, 55);
     }
-    
+
     /**
      * Bottle rotation Animation
      */
@@ -42,4 +42,4 @@ class ThrowableObject extends MovableObject{
             this.playAnimation(this.IMAGES_ROTATEBOTTLE);
         }, 120);
     }
-    }
+}
